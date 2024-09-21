99a1e317ccaffcf220e2572b81d2db69a36520b3
Gordon Olsen <gcolsenut@gmail.com>
2024-09-20 23:28:53 -0600

    more functions added for new card logic, and showing additional cards

--- index.js
+++ index.js
@@ -4,16 +4,29 @@
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
 let firstCard, secondCard, sum;
+let cards = [];
 let hasBlackjack = false;
 let isAlive = true;
 let message = '';
+let messageEL = document.getElementById('message-el');
+let sumEl = document.getElementById('sum-el');
+let cardsEl = document.getElementById('cards-el');
+let addCards = document.getElementById('additional-cards');
 
-firstCard = Math.floor(Math.random() * 12) + 2;
-secondCard = Math.floor(Math.random() * 12) + 2;
+firstCard = Math.floor(Math.random() * 11) + 2;
+secondCard = Math.floor(Math.random() * 11) + 2;
+cards = [firstCard, secondCard];
 
 sum = firstCard + secondCard;
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = 'Cards: ' + cards;
+  sumEl.textContent = 'Sum: ' + sum;
+
   if (sum <= 20) {
     message = 'Do you want to draw another card?';
   } else if (sum === 21) {
@@ -24,5 +37,17 @@ function startGame() {
     isAlive = false;
   }
 
-  console.log(message);
+  messageEL.textContent = message;
+}
+
+function newCard() {
+  console.log('Drawing another card from the deck');
+
+  let newCard = Math.floor(Math.random() * 11) + 2;
+
+  cards.push(newCard);
+
+  sum += newCard;
+
+  renderGame();
 }
